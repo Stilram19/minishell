dea1cb519393f6ca596c33d85a9c5e94e5e3b994
Omar Bednaoui <obednaou@e2r6p7.1337.ma>
2023-02-09 18:39:34 +0100

    first touch

--- utils.c
+++ utils.c
@@ -1,18 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: obednaou <obednaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/02/09 15:49:06 by obednaou          #+#    #+#             */
-/*   Updated: 2023/02/09 18:26:39 by obednaou         ###   ########.fr       */
+/*   Created: 2023/02/09 18:30:09 by obednaou          #+#    #+#             */
+/*   Updated: 2023/02/09 18:38:42 by obednaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(int argc, char **argv, char **env)
+t_list	*build_env(char **env)
 {
-	
+	int	i;
+
+	i = 0;
+	while (*(env + i))
+	{
+		
+	}
+}
+
+t_list	*get_env(char **env)
+{
+	static t_list	*my_env;
+
+	if (!my_env)
+		my_env = build_env(env);
+	return (my_env);
 }
