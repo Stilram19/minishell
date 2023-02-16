a61b911474d9e8f413553737cd71383c7da1f296
Omar Bednaoui <obednaou@e2r6p9.1337.ma>
2023-02-16 12:16:33 +0100

    syntax

--- check_syntax1.c
+++ check_syntax1.c
@@ -6,70 +6,35 @@
 /*   By: obednaou <obednaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/15 17:20:06 by obednaou          #+#    #+#             */
-/*   Updated: 2023/02/15 18:52:28 by obednaou         ###   ########.fr       */
+/*   Updated: 2023/02/16 12:16:22 by obednaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void	update_balance(char c, t_balance *b, char *brackets)
+void	update_balance(char c, t_balance *b)
 {
-	int	b;
-
-	b = 0;
-	while (*brackets)
+	if (c == '(' || c == ')')
 	{
-		if (c == *brackets)
+		if (b->top_i != -1 && c == ')' && b->stack[b->top_i] == '(')
 		{
-			b = 1;
-			break ;
+			b->top_i--;
+			return ;
 		}
-		brackets++;
-	}
-	if (!b)
-		return ;
-	if (b->top_i == -1 || c != b->stack[b->top_i])
-	{
 		b->top_i++;
 		b->stack[b->top_i] = c;
-		return ;
 	}
-	b->top_i--;
 }
 
-int	is_between_brackets(char *input_str)
+void	open_close_quotes(char ch, int *c)
 {
-	int			c;
-	t_balance	b;
-	char		*set;
-
-	c = 0;
-	b.top_i = -1;
-	set = "()";
-	b.stack = malloc(sizeof(char) * ft_strlen(input_str));
-	while (*input_str)
+	if (ch == '\'' || ch == '\"')
 	{
-		c = 1;
-		update_balance(*input_str, &b, set);
-		input_str++;
+		if (*c == 0)
+			*c = ch;
+		else if (*c == ch)
+			*c = 0;
 	}
-	free(b.stack);
-	return (c && (b.top_i == -1));
-}
-
-int	is_command(char *input_str)
-{
-	int	ret;
-
-	ret = 0;
-	while (*input_str)
-	{
-		ret = 1;
-		if (*input_str == '&' || *input_str == '|')
-			return (0);
-		input_str++;
-	}
-	return (ret);
 }
 
 int	is_operator(char *str)
@@ -80,110 +45,154 @@ int	is_operator(char *str)
 	return (0);
 }
 
-int	is_there_middle_operator(char *input_str)
+/*int	check_brackets_balance(char *str)
 {
-	int			c;
+	char		c;
 	t_balance	b;
-	char		*set;
 
 	c = 0;
 	b.top_i = -1;
-	set = "\'\"()";
-	b.stack = malloc(sizeof(ft_strlen(input_str)));
-	while (*input_str)
+	b.stack = ft_garbage_collector(ALLOCATE, ft_strlen(str), NULL);
+	while (*str)
 	{
-		update_balance(*input_str, &b, set);
-		if (c && b.top_i == -1 && is_operator(input_str))
+		if (*str == '\'' || *str == '\"')
 		{
-			free(b.stack);
-			return (1);
+			if (!c)
+				c = *str;
+			else if (c == *str)
+				c = 0;
 		}
-		c = 1;
-		input_str++;
+		else if (!c)
+			update_balance(*str, &b);
+		str++;
 	}
-	free(b.stack);
+	return (b.top_i == -1);
+}*/
+
+int	is_between_brackets(char *str, int len)
+{
+	if (*str == '(' && *(str + len - 1) == ')')
+		return (1);
 	return (0);
 }
 
-char	*first_operand(char *str)
+int	is_command(char *str)
 {
-	int			i;
-	int			c;
-	char		*set;
-	char		*ret;
-	t_balance	b;
-
-	c = 0;
-	i = 0;
-	set = "\'\"()";
-	ret = ft_strdup(str);
-	b.top_i = -1;
-	b.stack = malloc(sizeof(char) * ft_strlen(ret));
-	while (*(ret + i))
-	{
-		update_balance(*(ret + i), &b, set);
-		if (c && b.top_i == -1 && is_operator(ret + i))
-		{
-			*(ret + i) == '\0';
-			free(b.stack);
-			return (ret);
-		}
-		i++;
-		c = 1;
-	}
-	free(b.stack);
-	return (ret);
-}
-
-char	*second_operand(char *str)
-{
-	int			i;
-	int			c;
-	char		*set;
-	char		*ret;
-	t_balance	b;
-
-	c = 0;
-	i = 0;
-	b.top_i = -1;
-	set = "\'\"()";
-	b.stack = malloc(sizeof(char) * ft_strlen(str));
-	while (*(str + i))
-	{
-		update_balance(*(str + i), &b, set);
-		if (c && b.top_i == -1 && is_operator(ret + i))
-		{
-			ret = ft_strdup(str + i);
-			free(b.stack);
-			return (ret);
-		}
-		c = 1;
-	}
-	free(b.stack);
-	return (ret);
-}
-
-int	second_statement_symbol(char *input_str)
-{
+	int	c;
 	int	ret;
 
 	ret = 0;
-	if (is_command(input_str))
-		return (check_command_syntax(input_str));
-	if (is_between_brackets(input_str))
-		return (second_statement_symbol(input_str));
-	if (is_there_middle_operator(input_str))
+	c = 0;
+	while (*str)
 	{
-		ret = second_statement_symbol(first_operand(input_str));
-		(ret || (ret = second_statement_symbol(second_operand(input_str))));
+		ret = 1;
+		open_close_quotes(*str, &c);
+		if (!c && (*str == '&' || *str == '|'
+				|| *str == '(' || *str == ')'))
+			return (0);
+		str++;
+	}
+	return (ret);
+}
+
+
+int	is_there_middle_operator(char *str, int len)// ! check there is no operator at the beginning
+{
+	int			c;
+	t_balance	b;
+
+	c = 0;
+	b.top_i = -1;
+	b.stack = ft_garbage_collector(ALLOCATE, len, NULL);
+	while (*str)
+	{
+		open_close_quotes(*str, &c);
+		update_balance(*str, &b);
+		if (!c && b.top_i == -1 && is_operator(str))
+			return (1);
+		str++;
+	}
+	return (0);
+}
+
+char	*first_operand(char *str, int len)
+{
+	int			i;
+	int			c;
+	char		*ret;
+	t_balance	b;
+
+	c = 0;
+	i = 0;
+	b.top_i = -1;
+	ret = ft_strdup(str);
+	b.stack = ft_garbage_collector(ALLOCATE, len, NULL);
+	while (*(ret + i))
+	{
+		open_close_quotes(*(ret + i), &c);
+		if (!c)
+			update_balance(*(ret + i), &b);
+		if (!c && b.top_i == -1 && is_operator(ret + i))
+		{
+			*(ret + i) == '\0';
+			return (ret);
+		}
+		i++;
+	}
+	return (ret);
+}
+
+char	*second_operand(char *str, int len)
+{
+	int			c;
+	t_balance	b;
+
+	c = 0;
+	b.top_i = -1;
+	b.stack = ft_garbage_collector(ALLOCATE, len, NULL);
+	while (*str)
+	{
+		open_close_quotes(*str, &c);
+		if (!c)
+			update_balance(*str, &b);
+		if (!c && b.top_i == -1 && is_operator(str))
+		{
+			((*str == '&') && (str++));
+			break ;
+		}
+		str++;
+	}
+	return (str + 1);
+}
+
+int	second_statement_symbol(char *str, int len)
+{
+	int		ret;
+	char	*new_str;
+
+	ret = 0;
+	if (is_command(str))
+		return (check_command_syntax(str));
+	if (is_between_brackets(str, len))
+	{
+		str++;
+		*(str + len - 2) = '\0';
+		return (second_statement_symbol(str, len - 2));
+	}
+	if (is_there_middle_operator(str, len))
+	{
+		new_str = first_operand(str, len);
+		ret = second_statement_symbol(new_str, ft_strlen(new_str));
+		(ret || (new_str = second_operand(str, len)));
+		(ret || (ret = second_statement_symbol(new_str, ft_strlen(new_str))));
 		return (ret);
 	}
 	return (1);
 }
 
-int	first_statement_symbol(char *input_str)
+int	first_statement_symbol(char *input_str, int len)
 {
-	if (!(*input_str))
+	if (!len)
 		return (0);
-	return (second_statement_symbol(input_str));
+	return (second_statement_symbol(input_str, len));
 }
