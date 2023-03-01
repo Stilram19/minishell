61e9b2db2848b71445016416e934bf16e3c8db99
Omar Bednaoui <obednaou@e2r6p6.1337.ma>
2023-03-01 13:48:06 +0100

    working on ambig redirec

--- ambiguous_redirections.c
+++ ambiguous_redirections.c
@@ -6,7 +6,7 @@
 /*   By: obednaou <obednaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/27 16:08:07 by obednaou          #+#    #+#             */
-/*   Updated: 2023/02/27 19:39:58 by obednaou         ###   ########.fr       */
+/*   Updated: 2023/03/01 13:47:24 by obednaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,36 +19,74 @@
 
 int	is_all_blank(char *file_name)
 {
+	int	ret;
+	int	quotes;
+
+	ret = 0;
+	quotes = 0;
 	if (!(*file_name))
 		return (1);
-	while (is_blank(*file_name))
+	while (*file_name)
+	{
+		if (!is_blank(*file_name))
+			return (0);
 		file_name++;
-	return (!(*file_name));
+	}
+	return (1);
+}
+//""$var--$var--$var--$var
+//"empty"
+//""''""''""''""''""''""''""
+int	is_all_closed_quotes(char *file_name)
+{
+	int	quotes;
+
+	quotes = 0;
+	while (*file_name)
+	{
+		open_close_quotes(*file_name, &quotes);
+		if (!is_blank(*file_name) && *file_name != quotes)
+			return (0);
+		if ()
+		file_name++;
+	}
+	return (1);
 }
 
-int	is_ambiguous_redirect(char **ptr_to_file_name)
+int	split_and_check_ambiguity(char *file_name)
 {
 	int		i;
-	int		quotes;
-	int		blank;
-	char	*file_name;
+	int		ret;
+	char	**tokens;
 
 	i = 0;
-	quotes = 0;
-	blank = 0;
+	ret = 0;
+	tokens = produce_tokens(file_name, mask_generation1(file_name));
+	while (*(tokens + i))
+		i++;
+	*tokens = remove_quotes(*tokens);
+	if (i > 1 || !(*tokens))
+		ret = 1;
+	i = -1;
+	while (*(tokens + ++i))
+		ft_garbage_collector(SINGLE_RELEASE, 0, *(tokens + i));
+	ft_garbage_collector(SINGLE_RELEASE, 0, tokens);
+	return (ret);
+}
+
+// "a"$space
+int	is_ambiguous_redirect(char **ptr_to_file_name)
+{
+	int		ret;
+	char	*file_name;
+
+	ret = 0;
 	expand_if(ptr_to_file_name);
 	file_name = *ptr_to_file_name;
 	if (is_all_blank(file_name))
-		return (1);
-	file_name = remove_quotes(file_name);
-	while (*(file_name + i))
-	{
-		open_close_quotes(*(file_name + i), &quotes);
-		if (!quotes && is_blank(*(file_name + i)) && ++blank)
-			break ;
-		i++;
-	}
-	while (blank && is_blank(*(file_name + i)))
-		i++;
-	return (*(file_name + i));
+		ret = 1;
+	if (!ret && ft_strchr(file_name, " "))
+		ret = split_and_check_ambiguity(file_name);
+	*ptr_to_file_name = remove_quotes(file_name);
+	return (ret);
 }
--- general_utils1.c
+++ general_utils1.c
@@ -6,7 +6,7 @@
 /*   By: obednaou <obednaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/26 20:18:45 by obednaou          #+#    #+#             */
-/*   Updated: 2023/02/28 17:56:50 by obednaou         ###   ########.fr       */
+/*   Updated: 2023/02/28 18:48:41 by obednaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -90,5 +90,5 @@ char	*random_file_name_generation(void)
 			j++;
 		}
 	}
-	return (ft_strjoin("/tmp", random_name));
+	return (ft_strjoin("/tmp/", random_name));
 }
